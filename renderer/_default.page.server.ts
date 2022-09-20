c8aa54d32ed5cf241deba3eeb322da55c63aff70
Jonas Kuske <mail@jonaskuske.com>
2022-09-20 18:33:57 +0200

    feat: add favicon

--- renderer/_default.page.server.ts
+++ renderer/_default.page.server.ts
@@ -30,6 +30,14 @@ function render(pageContext: PageContextServer) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+        <link rel="manifest" href="/favicons/site.webmanifest">
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/favicons/favicon.ico">
+        <meta name="msapplication-TileColor" content="#2b5797">
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml">
       </head>
       <body>
         <div id="app">${pipeWrapper}</div>`
